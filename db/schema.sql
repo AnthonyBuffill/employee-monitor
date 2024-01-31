89e84df2394dc9300e810f0e383a4ee31b249a4f
Ant Buffill <abuffill@outlook.com>
2024-01-30 21:35:12 -0600

    began coding andn file building

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS courses_db;
+CREATE DATABASE courses_db;
+
+USE courses_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_ID INT,
+  FOREIGN KEY (department_ID)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_ID INT,
+  FOREIGN KEY (role_ID)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_ID INT,
+  FOREIGN KEY (manager_ID)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name)
+VALUES ("Web Development"),
+       ("Data Science"),
+       ("Math"),
+       ("Electives");
+
+INSERT INTO role (title, salary, department_ID)
+VALUES ("JavaScript Developer",60000.01, 1),
+       ("Data Scietist", 85000.03, 2),
+       ("Data analyst", 83000, 3),
+       ("Scrum master", 100002, 4),
+       ("Machine Learning technician", 5.00, 4);
+
+ 
+INSERT INTO employee (first_name, last_name, role_ID, manager_ID)
+VALUES ("Sandy", "Duncan", 1, NULL),
+       ("Jacob", "Carver", 5, 1), 
+       ("Daryl", "Revis", 4, 1), 
+       ("Yurgen", "Macadoo", 3, 1);
+
+             
+
+
+       
